10f82ef062b5440d0fcefb838acb85b626578bfe
Sarthak <notsarthakshah@gmail.com>
2024-07-11 21:39:33 +0530

    fix: redirection errors

--- client/app/(protected)/_components/navbar.tsx
+++ client/app/(protected)/_components/navbar.tsx
@@ -32,9 +32,9 @@ const Navbar = () => {
         </Button>
         <Button
           asChild
-          variant={pathname === "/dashboard/settings" ? "default" : "outline"}
+          variant={pathname === "/settings" ? "default" : "outline"}
         >
-          <Link href="/dashboard/settings">Settings</Link>
+          <Link href="/settings">Settings</Link>
         </Button>
       </div>
       <UserButton />
--- client/routes.ts
+++ client/routes.ts
@@ -31,4 +31,4 @@ export const apiAuthPrefix = "/api/auth";
 /**
 *   The default redirect path after logging in
 */
-export const DEFAULT_LOGIN_REDIRECT = "/dasbboard/settings";
+export const DEFAULT_LOGIN_REDIRECT = "/settings";
