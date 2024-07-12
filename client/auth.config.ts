9280f37c4681ec6e1af28df83d0c5486b6fff9e2
Manav <manav18gadhiya@gmail.com>
2024-07-12 18:33:54 +0530

    fix: social auth

--- client/auth.config.ts
+++ client/auth.config.ts
@@ -1,7 +1,7 @@
 import Credentials from "next-auth/providers/credentials";
 import Github from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
-import Twitch from "next-auth/providers/twitch"
+import Twitch from "next-auth/providers/twitch";
 import type { NextAuthConfig } from "next-auth";
 import { LoginSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
--- client/lib/mail.ts
+++ client/lib/mail.ts
@@ -2,45 +2,37 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export const sendTwoFactorTokenEmail = async(
-    email: string,
-    token: string
-) => {
-    await resend.emails.send({
-        from: "manav18gadhiya@gmail.com",
-        to: email,
-        subject: "2FA Code",
-        html: `<p>Your 2FA code: ${token}</p>`
-    })
-} 
+export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
+  await resend.emails.send({
+    from: "manav18gadhiya@gmail.com",
+    to: email,
+    subject: "2FA Code",
+    html: `<p>Your 2FA code: ${token}</p>`,
+  });
+};
 
-export const sendPasswordResetEmail = async (
-    email: string,
-    token: string,
-) => {
-    const resetLink = `http://localhost:3000/auth/new-password?token=${token}`
+export const sendPasswordResetEmail = async (email: string, token: string) => {
+  const resetLink = `http://localhost:3000/auth/new-password?token=${token}`;
 
-    await resend.emails.send({
-      from: "manav18gadhiya@gmail.com",
-      to: email,
-      subject: "Reset your password",
-      html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`,
-    });  
-}
+  await resend.emails.send({
+    from: "manav18gadhiya@gmail.com",
+    to: email,
+    subject: "Reset your password",
+    html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`,
+  });
+};
 
-export const sendVerificationEmail = async (
-    email: string,
-    token: string
-) => {
-    const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
+export const sendVerificationEmail = async (email: string, token: string) => {
+  const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
 
-    await resend.emails.send({
-        from: "manav18gadhiya@gmail.com",
-        to: email,
-        subject: "Confirm your email",
-        html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`
-    })
-}
+  await resend.emails.send({
+    from: "manav18gadhiya@gmail.com",
+    to: email,
+    subject: "Confirm your email",
+    html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`,
+  });
+};
+
 // import sgMail from "@sendgrid/mail";
 
 // sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
