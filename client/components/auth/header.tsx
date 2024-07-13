c6b712b2088c86ea894b1a7a7973d4f053f7bbf6
Sarthak <notsarthakshah@gmail.com>
2024-07-13 23:53:32 +0530

    fix: heading

--- client/components/auth/header.tsx
+++ client/components/auth/header.tsx
@@ -1,5 +1,7 @@
 import { Poppins } from "next/font/google";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+import logo from "@/public/black-logo.svg";
 
 const font = Poppins({
     subsets: ["latin"],
@@ -15,7 +17,13 @@ export const Header = ({
 }: HeaderProps) => {
     return (
       <div className="w-full flex flex-col gap-y-4 items-center justify-center">
-        <h1 className={cn("text-3xl font-semibold", font.className)}>🔐 Auth</h1>
+        <div className="flex gap-2 items-center">
+          <Image src={logo} alt="logo" width={30} height={30} />
+          <h1 className={cn("text-3xl font-semibold", font.className)}>
+            {" "}
+            Stellock
+          </h1>
+        </div>
         <p className="text-muted-foreground text-sm">{label}</p>
       </div>
     );
